3b1a7769a684a1cfe3624b8f3096a815f25376f1
TOKI Yoshinori <toki@freedom.ne.jp>
2019-06-13 11:18:41 +0900

    modified bundle gem template

---
+++ lib/rims/rfc822.rb
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+require 'rims/rfc822/version'
+
+module RIMS
+  module RFC822
+  end
+end
+
+# Local Variables:
+# mode: Ruby
+# indent-tabs-mode: nil
+# End:
---
+++ lib/rims/rfc822/version.rb
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+module RIMS
+  module RFC822
+    VERSION = '0.1.0'
+  end
+end
+
+# Local Variables:
+# mode: Ruby
+# indent-tabs-mode: nil
+# End:
